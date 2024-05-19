d5f982f269fce20bc3d0ce534a94d0977877a0ff
Your Name <you@example.com>
2024-05-19 11:48:07 +0300

    commit

--- huerga.py
+++ huerga.py
@@ -24,7 +24,7 @@ class Camera:
 class Detector:
     def __init__(self):
         self.model = YOLO("best.pt")
-        self.CONFIDENCE_THRESHOLD = 0.65
+        self.CONFIDENCE_THRESHOLD = 0.7
 
 
     def detection(self, frame):
@@ -37,6 +37,8 @@ class Detector:
                         continue
                     xmin,ymin, xmax, ymax, class_id = int(data[0]), int(data[1]), int(data[2]), int(data[3]), int(data[5])
                     results[class_id].append([xmin, ymin, xmax, ymax])
+                    # cv2.rectangle(frame, (xmin,ymin), (xmax, ymax), (0, 255, 0), 2)
+                    # cv2.putText(frame, f'{0}', (xmin,ymin), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), class_id)
         frame = self.analyze(frame, results)
         return frame
 
@@ -49,14 +51,22 @@ class Detector:
                 cv2.putText(frame, f'{0}', (i[0], i[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red, 2)
         else:
             for person in detections[0]:
+                hats = []
+                personCenter = person[0]+(person[2] - person[0])/2, person[1]+(person[3] - person[1])/2
                 for hat in detections[1]:
                     hatCenter = (hat[0]+(hat[2] - hat[0])/2, hat[1]+(hat[3] - hat[1])/2)
-                    if hatCenter[0] >= person[0] and hatCenter[0] <= person[2] and  hatCenter[1] >= person[1] and hatCenter[1] <= person[3]:
-                        cv2.rectangle(frame, (person[0], person[1]), (person[2], person[3]), green, 2)
-                        cv2.putText(frame, f'{0}', (person[0], person[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, green, 2)
-                    else:
-                        cv2.rectangle(frame, (person[0], person[1]), (person[2], person[3]), red, 2)
-                        cv2.putText(frame, f'{0}', (person[0], person[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red, 2)
+                    hatDist = np.sqrt((personCenter[0] - hatCenter[0])**2+(personCenter[1] - hatCenter[1])**2)
+                    hats.append(hat.copy()+[hatDist])
+
+                hats = sorted(hats, key = lambda x : x[4],reverse=False)
+                hat = hats[0]
+                hatCenter = (hat[0]+(hat[2] - hat[0])/2, hat[1]+(hat[3] - hat[1])/2)
+                if hatCenter[0] >= person[0] and hatCenter[0] <= person[2] and  hatCenter[1] >= person[1] and hatCenter[1] <= person[3]:
+                    cv2.rectangle(frame, (person[0], person[1]), (person[2], person[3]), green, 2)
+                    cv2.putText(frame, f'{0}', (person[0], person[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, green, 2)
+                else:
+                    cv2.rectangle(frame, (person[0], person[1]), (person[2], person[3]), red, 2)
+                    cv2.putText(frame, f'{0}', (person[0], person[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red, 2)
         return frame       
 
 
@@ -69,7 +79,7 @@ class Main:
             try:
                 detected_frame = self.detector.detection(next(generator))
                 cv2.imshow('image', detected_frame)
-                cv2.waitKey(1000)
+                cv2.waitKey(3000)
             except StopIteration:
                 break
 
