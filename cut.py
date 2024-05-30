288ecdc07618080ac68191c02ec53a862e2f0f7e
Mashyanovk <mashyanov.kk@edu.spbstu.ru>
2024-05-30 21:57:15 +0300

    commit

--- cut.py
+++ cut.py
@@ -3,10 +3,14 @@ import cv2
 
 cap = cv2.VideoCapture('video.mp4')
 i=0
+j=0
 while True:
     ret,frame = cap.read()
     if not ret:
         break
-    cv2.imwrite(f'images_from_video/{i}.jpg',frame)
-    cv2.waitKey(1)
+    if j==29:
+        cv2.imwrite(f'images_from_video1/{i}.jpg',frame)
+        cv2.waitKey(1)
+        j=0
+    j+=1
     i+=1
